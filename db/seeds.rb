dd6d8a98812399f08364e2ca06b3a7f91cb540fa
michelgrootjans <michelgrootjans@michel-grootjanss-macbook-pro.local>
2011-09-03 20:33:41 +0200

    Added cart and line_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Product.delete_all
 Product.create(title: 'Programming Ruby 1.9',
                description: %{<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast,  you should add Ruby to your toolbox. </p>},
-               image_url: 'ruby.jpg',
+               image_url: 'http://fastcache.gawkerassets.com/assets/images/4/2011/09/xsmall_quantum_01.jpg',
                price: 49.95)
