b242e9533ac86078ad3fbca250ea5ced56784286
michelgrootjans <michelgrootjans@michel-grootjanss-macbook-pro.local>
2011-09-04 16:45:06 +0200

    ajax-ified the shopping cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Product.delete_all
 Product.create(title: 'Programming Ruby 1.9',
                description: %{<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast,  you should add Ruby to your toolbox. </p>},
-               image_url: 'http://fastcache.gawkerassets.com/assets/images/4/2011/09/xsmall_quantum_01.jpg',
+               image_url: 'ruby.jpg',
                price: 49.95)
 Product.create(title: 'Ruby for the .net developer',
                description: %{<p>You wanna leave the Micro$oft ecosystem. Here's how.</p>},
-               image_url: 'http://fastcache.gawkerassets.com/assets/images/4/2011/09/xsmall_quantum_01.jpg',
+               image_url: 'rtp.jpg',
                price: 49.95)
