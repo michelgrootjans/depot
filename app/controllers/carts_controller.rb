24e6242598051ddee87a5eecc70e2406676e02fa
michelgrootjans <michelgrootjans@michel-grootjanss-macbook-pro.local>
2011-09-04 12:50:51 +0200

    cleanup carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,7 @@
 class CartsController < ApplicationController
-  def index
-    @carts = Cart.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @carts }
-    end
-  end
-
   def show
     begin
-      @cart = Cart.find(params[:id])
+      @cart = current_cart
     rescue ActiveRecord::RecordNotFound
       logger.error "Attempt to access an invalid cart #{params[:id]}"
       redirect_to store_url, notice: "Invalid cart"
@@ -22,53 +13,12 @@ class CartsController < ApplicationController
     end
   end
 
-  def new
-    @cart = Cart.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @cart }
-    end
-  end
-
-  def edit
-    @cart = Cart.find(params[:id])
-  end
-
-  def create
-    @cart = Cart.new(params[:cart])
-
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render json: @cart, status: :created, location: @cart }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      if @cart.update_attributes(params[:cart])
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_path }
       format.json { head :ok }
     end
   end
