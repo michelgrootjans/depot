336e4b2db4ed4182e63acb1911006e1f402633f8
michelgrootjans <michelgrootjans@michel-grootjanss-macbook-pro.local>
2011-09-05 12:43:08 +0200

    demo voor steven

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -8,7 +8,9 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to store_url }
         format.js   { @current_item = @line_item }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.json { render json: @line_item, 
+                      status: :created,
+                      location: @line_item }
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
