e2fb54ed6e0bc70a1418c9c6f2c1da85bf22f132
michelgrootjans <michelgrootjans@michel-grootjanss-macbook-pro.local>
2011-09-11 13:52:39 +0200

    user CRUD

---
+++ test/unit/user_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "username cannot be empty" do
+    user = User.new(name: "", password_digest: "secret")
+    assert user.invalid?
+    assert user.errors[:name].any?
+  end
+
+  test "username must be unique" do
+    user = User.new(name: "scott", password_digest: "secret")
+    assert !user.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+                 user.errors[:name].join('; ')
+  end
+end
